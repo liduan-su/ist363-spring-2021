1d1fe7672c9acfac5fa67df814e92c4b65c49c9f
Liwen Duan <hi@liwenduan.com>
2021-05-20 11:18:49 -0400

    v1 of Location

--- pages/locations/[id].js
+++ pages/locations/[id].js
@@ -1,11 +1,16 @@
 import Layout from '../../components/layout';
 import Image from 'next/image';
 import Link from 'next/link';
-import {getAllLocationSlugs, getLocationItemBySlug} from '../../lib/api';
+import Section from '../../components/section';
+import Card from '../../components/card';
+import Row from '../../components/row';
+import Col from '../../components/col';
+import Heading from '../../components/heading';
+import {getLocationItems, getLocationItemBySlug} from '../../lib/api';
 
 
 export async function getStaticPaths() {
-    const allSlugs = await getAllLocationSlugs();
+    const allSlugs = await getLocationItems();
     const paths = allSlugs.edges.map(edge => {
         const {slug} = edge.node;
         return {
@@ -31,8 +36,8 @@ export async function getStaticProps({params}) {
 }
 
 export default function LocationItem({locationItemData}) {
-    const {title, featuredImage, content} = locationItemData;
-    const {sourceUrl, mediaDetails, altText} = featuredImage;
+    const {title, featuredImage, content, menuTypes} = locationItemData;
+    const {sourceUrl, mediaDetails, altText} = featuredImage.node;
     const {width,height} = mediaDetails;
 
     return (
@@ -48,6 +53,22 @@ export default function LocationItem({locationItemData}) {
             />
             <h1>{title}</h1>
             <div dangerouslySetInnerHTML={{__html: content}}/>
+            <Heading type="h1">Menu</Heading>
+            {menuTypes.edges.map(edge=> {
+                const {name, items} = edge.node;
+                return <Section title={name}>
+                    <Row justifyContentCenter>
+                    {items.edges.map((edge, index)=> {
+                        const {node} = edge;
+                        return <Col sm={6} md={4} lg={3} key={index}>
+                            <Card node={node} returnPage="menu"/>  
+                        </Col>
+                    })}
+                    </Row>
+                </Section>
+            })}
+            <Heading type="h1">People</Heading>
+            
         </Layout>
     )
 }
